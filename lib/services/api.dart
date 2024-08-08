1d66b66fa03b52d41c2f2f6cbf463e876338fc65
Andima <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>
2024-08-08 22:12:50 +0100

    F/all

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,6 +1,6 @@
 class Api {
   Api._();
-  static const baseUrl = "http://hng-task2.randommallstudio.tech/api/v1/";
+  static const baseUrl = "https://ws.python.hng.tech/api/v1/";
   static const login = "auth/login";
   static const signup = "auth/register";
   static const user = "users/me";
