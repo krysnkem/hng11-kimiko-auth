1795637866e5cebeacfbab14731611b6d11d1394
Christian Onyisi <krysnkem@gmail.com>
2024-08-09 17:16:05 +0100

    handle loading action on profile

--- example/lib/profile.dart
+++ example/lib/profile.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:kimko_auth/services/kimiko_exeception.dart';
 
@@ -12,7 +14,6 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
   bool _isLoading = false;
 
   startLoading() {
@@ -32,11 +33,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
     try {
       var res = await kimkoAuth.logOut();
       print(res.data);
-      setState(() {
-        isLoggedIn.value = false;
-        user.value ={};
-      });
-      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (_)=> const LoginScreen()), (route) => false);
+      isLoggedIn.value = false;
+      user.value = {};
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (_) => const LoginScreen()),
+          (route) => false);
     } on KimikoException catch (e) {
       debugPrint('Kimiko ${e.error}');
     } catch (e) {
@@ -51,7 +52,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
     try {
       var res = await kimkoAuth.getUser();
       print(res.data);
-      if(res.data != null){
+      if (res.data != null) {
+        log('${res.data}');
         setState(() {
           user.value = res.data;
         });
@@ -65,6 +67,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     }
     stopLoading();
   }
+
   @override
   void initState() {
     getSavedUser();
@@ -75,7 +78,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
     startLoading();
     try {
       var res = await kimkoAuth.getLoggedInUser();
-      if(res.data != null){
+      log('${res.data?.toString()}');
+      if (res.data != null) {
         setState(() {
           user.value = res.data;
         });
@@ -94,108 +98,122 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Hero(
-          tag: "APPBAR",
-          child: const Text('User Page')
-        ),
+        title: const Hero(tag: "APPBAR", child: Text('User Page')),
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView(
-                    children: [
-                      Hero(
-                        tag: "PROFILE_PIC",
-                        child: Container(
-                          height: 300,
-                          width: 300,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(user.value["avatar_url"]??"")
-                              )
+      body: Builder(builder: (context) {
+        if (_isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        Hero(
+                          tag: "PROFILE_PIC",
+                          child: Container(
+                            height: 300,
+                            width: 300,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                        user.value["avatar_url"] ?? ""))),
                           ),
                         ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        UserDetails(
+                          title: "First Name",
+                          body: user.value["first_name"],
+                        ),
+                        UserDetails(
+                          title: "Last Name",
+                          body: user.value["last_name"],
+                        ),
+                        UserDetails(
+                          title: "Email",
+                          body: user.value["email"],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Hero(
+                          tag: "SUBMIT_BUTTON",
+                          child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8))
+                                  // shape:
+                                  ),
+                              onPressed: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (_) =>
+                                          const EditProfileScreen())),
+                              child: const Text("Edit Profile")),
+                        ),
                       ),
-                      const SizedBox(height: 16,),
-                      UserDetails(
-                        title: "First Name",
-                        body: user.value["first_name"],
+                      const SizedBox(
+                        width: 16,
                       ),
-                      UserDetails(
-                        title: "Last Name",
-                        body: user.value["last_name"],
+                      Expanded(
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8))
+                                // shape:
+                                ),
+                            onPressed: getUser,
+                            child: const Text("Get User")),
                       ),
-                      UserDetails(
-                        title: "Email",
-                        body: user.value["email"],
-                      ),
-
                     ],
                   ),
-                ),
-                const SizedBox(height: 16,),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Hero(
-                        tag: "SUBMIT_BUTTON",
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8)
-                            )
-                            // shape:
-                          ),
-                          onPressed: ()=> Navigator.of(context).push(MaterialPageRoute(builder: (_)=> const EditProfileScreen())),
-                          child: const Text("Edit Profile")
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 16,),
-                    Expanded(
-                      child: OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)
-                          )
-                          // shape:
-                        ),
-                        onPressed: getUser,
-                        child: const Text("Get User")
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10,),
-                OutlinedButton(
-                    onPressed: ()=> logOut(context),
-                    child: const Text("Logout")),
-                const SizedBox(height: 16,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)
-                    )
-                    // shape:
+                  const SizedBox(
+                    height: 10,
                   ),
-                  onPressed: () async {
-
-                  },
-                  child: const Text("Deactivate Account", style: TextStyle(fontWeight: FontWeight.w700, color: Colors.white),)
-                ),
-                const SizedBox(height: 30,),
-              ],
+                  OutlinedButton(
+                      onPressed: () => logOut(context),
+                      child: const Text("Logout")),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8))
+                          // shape:
+                          ),
+                      onPressed: () async {},
+                      child: const Text(
+                        "Deactivate Account",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, color: Colors.white),
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
             ),
-          ),
-          if (_isLoading) const LoaderScreen()
-        ],
-      ),
+            if (_isLoading) const LoaderScreen()
+          ],
+        );
+      }),
     );
   }
 }
